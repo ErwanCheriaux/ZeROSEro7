ed5df3588d8e89b0b475de4e894032f1cd09ed87
Erwan CHERIAUX <erwan.cheriaux@telecom-paristech.fr>
2017-11-08 16:31:47 +0100

    test fibo function

--- test/fibo/main.c
+++ test/fibo/main.c
@@ -9,11 +9,26 @@
 
 #define NB_TEST 10
 
+static int fibo_result[NB_TEST];
+
 int main(void)
 {
-    for(int i=0; i<NB_TEST; i++)
-        printf("fibo_static(%d) = %d\n", i, fibo_static(i));
+    fibo_result[0] = 1;
+    fibo_result[1] = 1;
+    fibo_result[2] = 2;
+    fibo_result[3] = 3;
+    fibo_result[4] = 5;
+    fibo_result[5] = 8;
+    fibo_result[6] = 13;
+    fibo_result[7] = 21;
+    fibo_result[8] = 34;
+    fibo_result[9] = 55;
 
     for(int i=0; i<NB_TEST; i++)
-        printf("fibo_recursive(%d) = %d\n", i, fibo_recursive(i));
+        if(fibo_static(i) != fibo_result[i]) return 1;
+
+    for(int i=0; i<NB_TEST; i++)
+        if(fibo_recursive(i) != fibo_result[i]) return 1;
+
+    return 0;
 }
