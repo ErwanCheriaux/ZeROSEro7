dada7867479da03490638f8cc8bc47ee1b88cfa0
Enguerrand de RIBAUCOURT <deribaucourt@telecom-paristech.fr>
2017-11-21 17:00:04 +0100

    Error handling by printing on RTT for soft device errors

--- nrf52_dev_kit/rtt.h
+++ nrf52_dev_kit/rtt.h
@@ -5,8 +5,15 @@
 
 #define rtt_init SEGGER_RTT_Init
 #define rtt_write_string(a) SEGGER_RTT_WriteString(0,a)
+
+
+/* Printf signature : int SEGGER_RTT_printf(unsigned BufferIndex, const char * sFormat, ...);
+Put 0 as first argument for default channel 0 */
 #define rtt_printf SEGGER_RTT_printf
-// Printf signature : int SEGGER_RTT_printf(unsigned BufferIndex, const char * sFormat, ...);
-// Put 0 as first argument for default channel 0
+
+/* Called by APP_ERROR_CHECK(err_code) if err_code != NRF_SUCCESS.
+info is actually an error_info_t
+*/
+void app_error_fault_handler(uint32_t id, uint32_t pc, uint32_t info) ;
 
 #endif
