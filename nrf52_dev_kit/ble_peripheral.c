72c4830f9d6fc7d80e14299512704a49189ebab2
Enguerrand de RIBAUCOURT <deribaucourt@telecom-paristech.fr>
2017-11-23 17:14:07 +0100

    Advertising failed because of invalid param

--- nrf52_dev_kit/ble_peripheral.c
+++ nrf52_dev_kit/ble_peripheral.c
@@ -1,27 +1,107 @@
+#include "nordic_common.h"
+#include "nrf.h"
+#include "app_error.h"
+#include "ble_hci.h"
 #include "ble_gap.h"
+#include "ble_err.h"
 #include "ble_advdata.h"
 #include "ble_advertising.h"
-
 #include "nrf_sdh.h"
 #include "nrf_sdh_ble.h"
+#include "nrf_sdh_soc.h"
+#include "ble_srv_common.h"
+#include "ble_conn_params.h"
+#include "ble_db_discovery.h"
+#include "ble_ans_c.h"
+#include "app_timer.h"
+#include "bsp_btn_ble.h"
+#include "peer_manager.h"
+#include "fds.h"
+#include "ble_conn_state.h"
+#include "nrf_ble_gatt.h"
+
+#include "nrf_log.h"
+#include "nrf_log_ctrl.h"
+#include "nrf_log_default_backends.h"
 
 #include "rtt.h"
 
-#define APP_BLE_OBSERVER_PRIO           3                                           /**< Application's BLE observer priority. You shouldn't need to modify this value. */
-#define APP_BLE_CONN_CFG_TAG            1                                           /**< A tag identifying the SoftDevice BLE configuration. */
 
 
 
-/**@brief Function for handling BLE events.
- *
- * @param[in]   p_ble_evt   Bluetooth stack event.
- * @param[in]   p_context   Unused.
- */
-static void ble_evt_handler(ble_evt_t const * p_ble_evt, void * p_context) {
+// BLE Params
+#define APP_BLE_OBSERVER_PRIO 3 /**< Application's BLE observer priority. You shouldn't need to modify this value. */
+#define APP_BLE_CONN_CFG_TAG 1 /**< A tag identifying the SoftDevice BLE configuration. */
 
+// Advertising Params
+// Advertising is in slow mode since no power management is required in the dev kit
+#define DEVICE_NAME "Gatt Parrot"
+#define APP_ADV_FAST_INTERVAL 40 /**< The advertising interval (in units of 0.625 ms. This value corresponds to 25 ms). */
+#define APP_ADV_FAST_TIMEOUT 150 /**< The duration of the fast advertising period (in seconds). */
+
+
+
+/**< GATT module instance. */
+BLE_ADVERTISING_DEF(advertising_instance);
+
+
+
+// Advertising
+static void on_adv_evt(ble_adv_evt_t ble_adv_evt)
+{
+    ret_code_t err_code;
+
+    switch (ble_adv_evt)
+    {
+        case BLE_ADV_EVT_DIRECTED:
+            rtt_write_string("Directed advertising\n");
+            break;
+
+        case BLE_ADV_EVT_FAST:
+            rtt_write_string("Fast advertising\n");
+            /*err_code = bsp_indication_set(BSP_INDICATE_ADVERTISING);
+            APP_ERROR_CHECK(err_code);*/
+            break;
+
+        case BLE_ADV_EVT_SLOW:
+            rtt_write_string("Slow advertising\n");
+            break;
+
+        case BLE_ADV_EVT_IDLE:
+            break;
+
+        default:
+            break;
+    }
 }
 
-static void ble_stack_init(void)
+ble_advertising_init_t advertising_conf ;
+static void advertising_init() {
+    ret_code_t err_code;
+
+    advertising_conf.advdata.name_type               = BLE_ADVDATA_FULL_NAME;
+    advertising_conf.advdata.include_appearance      = true;
+    advertising_conf.advdata.flags                   = BLE_GAP_ADV_FLAGS_LE_ONLY_GENERAL_DISC_MODE;
+    advertising_conf.advdata.uuids_complete.uuid_cnt = 0;
+    advertising_conf.advdata.uuids_complete.p_uuids  = NULL;
+
+    advertising_conf.config.ble_adv_fast_enabled  = true;
+    advertising_conf.config.ble_adv_fast_interval = APP_ADV_FAST_INTERVAL;
+    advertising_conf.config.ble_adv_fast_timeout  = APP_ADV_FAST_TIMEOUT;
+
+    advertising_conf.evt_handler = on_adv_evt;
+
+	err_code = ble_advertising_init(&advertising_instance, &advertising_conf) ;
+    APP_ERROR_CHECK(err_code);
+}
+
+
+// BLE Basic
+static void ble_evt_handler(ble_evt_t const * p_ble_evt, void * p_context) {
+    rtt_write_string("BLE Event\n") ;
+}
+
+static void ble_stack_init()
 {
     ret_code_t err_code;
 
@@ -41,9 +121,10 @@ static void ble_stack_init(void)
 
 void ble_init(void) {
     ble_stack_init();
-//    advertising_init();
+    advertising_init();
 }
 
 void ble_start_advertising() {
-//    advertising_start(erase_bonds);
+    ret_code_t err_code = ble_advertising_start(&advertising_instance, BLE_ADV_MODE_FAST);
+    APP_ERROR_CHECK(err_code);
 }
--- nrf52_dev_kit/rtt.c
+++ nrf52_dev_kit/rtt.c
@@ -1,11 +1,51 @@
-#include "rtt.h"
+#include "nrf_sdh.h"
+#include "nrf_sdh_ble.h"
 #include "app_error.h"
+#include "nordic_common.h"
+#include "nrf.h"
+#include "ble_hci.h"
+#include "ble_gap.h"
+
+#include "rtt.h"
 
 void app_error_fault_handler(uint32_t id, uint32_t pc, uint32_t info) {
     error_info_t * error_info = (error_info_t *) info ;
     rtt_write_string("\n******** SOFT DEVICE ERROR OCCURED ********\n") ;
-    rtt_printf(0, "Error code %#04X ", error_info->err_code) ;
-    rtt_printf(0, "in file %s:%u\n", error_info->p_file_name, error_info->line_num) ;
+    rtt_printf(0, "Error code %#08X ", error_info->err_code) ;
+
+    /*│1462     * @retval ::NRF_SUCCESS The BLE stack has started advertising.                                                                                          │
+     │1463     * @retval ::NRF_ERROR_INVALID_ADDR Invalid pointer supplied.                                                                                            │
+     │1464     * @retval ::NRF_ERROR_INVALID_STATE Invalid state to perform operation.                                                                                 │
+     │1465     * @retval ::NRF_ERROR_CONN_COUNT The limit of available connections has been reached; connectable advertiser cannot be started.                         │
+     │1466     * @retval ::NRF_ERROR_INVALID_PARAM Invalid parameter(s) supplied, check the accepted ranges and limits.                                                │
+     │1467     * @retval ::BLE_ERROR_GAP_INVALID_BLE_ADDR Invalid Bluetooth address supplied.                                                                          │
+     │1468     * @retval ::BLE_ERROR_GAP_DISCOVERABLE_WITH_WHITELIST Discoverable mode and whitelist incompatible.                                                     │
+     │1469     * @retval ::NRF_ERROR_RESOURCES*/
+    switch(error_info->err_code) {
+        case NRF_ERROR_INVALID_ADDR:
+            rtt_write_string("NRF_ERROR_INVALID_ADDR\n") ;
+            break ;
+        case NRF_ERROR_INVALID_STATE:
+            rtt_write_string("NRF_ERROR_INVALID_STATE\n") ;
+            break ;
+        case NRF_ERROR_CONN_COUNT:
+            rtt_write_string("NRF_ERROR_CONN_COUNT\n") ;
+            break ;
+        case NRF_ERROR_INVALID_PARAM:
+            rtt_write_string("NRF_ERROR_INVALID_PARAM\n") ;
+            break ;
+        case BLE_ERROR_GAP_INVALID_BLE_ADDR:
+            rtt_write_string("BLE_ERROR_GAP_INVALID_BLE_ADDR\n") ;
+            break ;
+        case BLE_ERROR_GAP_DISCOVERABLE_WITH_WHITELIST:
+            rtt_write_string("BLE_ERROR_GAP_DISCOVERABLE_WITH_WHITELIST\n") ;
+            break ;
+        case NRF_ERROR_RESOURCES:
+            rtt_write_string("NRF_ERROR_RESOURCES\n") ;
+            break ;
+        default:
+            break ;
+    }
     rtt_printf(0, "See components/librairies/util/sdk_errors.h for more details on the Error Code \n") ;
     rtt_write_string("Stopping execution.\n") ;
 
