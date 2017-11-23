d32fa7f9a5cd1e9c6dc3b9ffc20a65d285c9d9ec
deribaucourt <deribaucourt@telecom-paristech.fr>
2017-11-24 00:36:39 +0100

    phone was advertising in BL, not BLE. Working app to advertise on BLE

--- smartphone/BLEParrot/app/src/main/java/fr/telecom_paristech/bleparrot/MainActivity.java
+++ smartphone/BLEParrot/app/src/main/java/fr/telecom_paristech/bleparrot/MainActivity.java
@@ -2,12 +2,16 @@ package fr.telecom_paristech.bleparrot;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.content.Intent ;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        super.onCreate(savedInstanceState) ;
+        setContentView(R.layout.activity_main) ;
+
+        Intent advertisementIntent = new Intent(this, Advertiser.class) ;
+        startService(advertisementIntent) ;
     }
 }
