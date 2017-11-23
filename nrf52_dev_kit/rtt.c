effed6dea46e4d664a1f652422234a605f62f238
Enguerrand de RIBAUCOURT <deribaucourt@telecom-paristech.fr>
2017-11-23 20:23:32 +0100

    TODO investigate scanning phones and BLE parameters

--- nrf52_dev_kit/rtt.c
+++ nrf52_dev_kit/rtt.c
@@ -14,7 +14,7 @@ void app_error_fault_handler(uint32_t id, uint32_t pc, uint32_t info) {
     rtt_write_string("\n******** SOFT DEVICE ERROR OCCURED ********\n") ;
     rtt_printf(0, "Error code 0x%#08X : ", error_info->err_code) ;
     rtt_write_string(nrf_strerror_get(error_info->err_code)) ;
-    rtt_write_string("Stopping execution.\n") ;
+    rtt_write_string("\nStopping execution.\n") ;
 
     app_error_save_and_stop(id, pc, info) ;
 }
