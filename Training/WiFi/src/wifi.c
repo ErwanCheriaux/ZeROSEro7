027c97b5708f57ab9d53c47f3307cf6fe67183b1
Vincent Gaillard <vincent.gaillard@telecom-paristech.fr>
2017-12-05 15:30:47 +0100

    handle wifi command errors and init wifi white list

--- Training/WiFi/src/wifi.c
+++ Training/WiFi/src/wifi.c
@@ -5,24 +5,46 @@
 #include "uart.h"
 #include "rtt.h"
 
-#define DEBUG 1
+// init white list
+const char white_list[2][65] = {
+    "c26d99fa3977e96955f8678cfb6c611414bcb4b6d9395166dacd9acf6c2d873e",
+    "debd23d6f2a553f2a003d3423d5737e35f6bb897ddad48fac30a3b0b5d210568"
+};
 
 void wifi_init(void)
 {
     uart_init();
 }
 
-void wifi_send(void* buff)
+int wifi_send(void* buff)
 {
     int     header_len = 9 + strlen(buff);
     uint8_t header_buffer[header_len];
     rtt_printf(0, ">%s", buff);
     uart_send(buff);
-// get header
+    // get header
 #ifdef DEBUG
     rtt_printf(0, "Header len: %d\n", header_len);
 #endif
     uart_receive(header_buffer, header_len);
+    // check error code
+    int err_code = header_buffer[header_len - 9] - '0';
+    if(err_code != 0) {
+        rtt_printf(0, "[ERROR] An error occured after sending this command: \"%s\":\n", buff);
+        rtt_printf(0, "[ERROR] Error code:%d (", err_code);
+        switch(err_code) {
+            case 1: rtt_printf(0, "Command failed)\n");
+            case 2: rtt_printf(0, "Parse error)\n");
+            case 3: rtt_printf(0, "Unknown command)\n");
+            case 4: rtt_printf(0, "Too few args)\n");
+            case 5: rtt_printf(0, "Too many args)\n");
+            case 6: rtt_printf(0, "Unknown variable or option)\n");
+            case 7: rtt_printf(0, "Invalid argument)\n");
+            case 8: rtt_printf(0, "Serial command line buffer overflow)\n");
+            case 9: rtt_printf(0, "Bounds error, command specific, bounds of the command were exceeded)\n");
+        }
+        return err_code;
+    }
     // extract data length from header
     int data_len = 0;
     for(int i = header_len - 8; i <= header_len - 3; i++)
@@ -44,4 +66,5 @@ void wifi_send(void* buff)
         data_len -= data_buff_len;
     }
     rtt_printf(0, "\n\n", data_buffer);
+    return 0;
 }
--- Training/WiFi/src/wifi.h
+++ Training/WiFi/src/wifi.h
@@ -8,11 +8,17 @@
 
 #define MAX_DATA_BUFFER_LEN 256
 
+/* Allow Network SSID (SHA3-256)
+** Only these ID will be allowed to connect to this device
+*/
+extern const char white_list[2][65];
+
 void wifi_init(void);
 
 /* Send data using uart
-** buff: message to send buffer
+** buff:   message to send buffer
+** return: error code (0 means success)
 */
-void wifi_send(void* buff);
+int wifi_send(void* buff);
 
 #endif
