658bbb09d1208a0b99bda50bb1a556c36de69420
Erwan CHERIAUX <erwan.cheriaux@telecom-paristech.fr>
2018-01-10 17:51:58 +0100

    string_descriptor valid

--- Training/USB_keyboard/src/usb.c
+++ Training/USB_keyboard/src/usb.c
@@ -171,42 +171,29 @@ static const uint8_t vcom_string0[] = {
  * Vendor string.
  *
  * STRING DESCRIPTOR
- *  bLength: 48
+ *  bLength: 10
  *  bDescriptorType: 0x03 (STRING)
- *  bString: Dell USB Entry Keyboard
+ *  bString: DELL
  */
 static const uint8_t vcom_string1[] = {
-    USB_DESC_BYTE(38),                    /* bLength.                         */
+    USB_DESC_BYTE(10),                    /* bLength.                         */
     USB_DESC_BYTE(USB_DESCRIPTOR_STRING), /* bDescriptorType.                 */
-    'S', 0, 'T', 0, 'M', 0, 'i', 0, 'c', 0, 'r', 0, 'o', 0, 'e', 0,
-    'l', 0, 'e', 0, 'c', 0, 't', 0, 'r', 0, 'o', 0, 'n', 0, 'i', 0,
-    'c', 0, 's', 0};
+    'D', 0, 'E', 0, 'L', 0, 'L', 0};
 
 /*
  * Device Description string.
  *
  * STRING DESCRIPTOR
- *  bLength: 10
+ *  bLength: 48
  *  bDescriptorType: 0x03 (STRING)
- *  bString: DELL
+ *  bString: Dell USB Entry Keyboard
  */
 static const uint8_t vcom_string2[] = {
-    USB_DESC_BYTE(56),                    /* bLength.                         */
+    USB_DESC_BYTE(48),                    /* bLength.                         */
     USB_DESC_BYTE(USB_DESCRIPTOR_STRING), /* bDescriptorType.                 */
-    'C', 0, 'h', 0, 'i', 0, 'b', 0, 'i', 0, 'O', 0, 'S', 0, '/', 0,
-    'R', 0, 'T', 0, ' ', 0, 'V', 0, 'i', 0, 'r', 0, 't', 0, 'u', 0,
-    'a', 0, 'l', 0, ' ', 0, 'C', 0, 'O', 0, 'M', 0, ' ', 0, 'P', 0,
-    'o', 0, 'r', 0, 't', 0};
-
-/*
- * Serial Number string.
- */
-static const uint8_t vcom_string3[] = {
-    USB_DESC_BYTE(8),                     /* bLength.                         */
-    USB_DESC_BYTE(USB_DESCRIPTOR_STRING), /* bDescriptorType.                 */
-    '0' + CH_KERNEL_MAJOR, 0,
-    '0' + CH_KERNEL_MINOR, 0,
-    '0' + CH_KERNEL_PATCH, 0};
+    'D', 0, 'e', 0, 'l', 0, 'l', 0, ' ', 0, 'U', 0, 'S', 0, 'B', 0,
+    ' ', 0, 'E', 0, 'n', 0, 't', 0, 'r', 0, 'y', 0, ' ', 0, 'K', 0,
+    'e', 0, 'y', 0, 'b', 0, 'o', 0, 'a', 0, 'r', 0, 'd', 0};
 
 /*
  * Strings wrappers array.
@@ -214,8 +201,7 @@ static const uint8_t vcom_string3[] = {
 static const USBDescriptor vcom_strings[] = {
     {sizeof vcom_string0, vcom_string0},
     {sizeof vcom_string1, vcom_string1},
-    {sizeof vcom_string2, vcom_string2},
-    {sizeof vcom_string3, vcom_string3}};
+    {sizeof vcom_string2, vcom_string2}};
 
 /*
  * Handles the GET_DESCRIPTOR callback. All required descriptors must be
@@ -234,7 +220,7 @@ static const USBDescriptor *get_descriptor(USBDriver *usbp,
         case USB_DESCRIPTOR_CONFIGURATION:
             return &vcom_configuration_descriptor;
         case USB_DESCRIPTOR_STRING:
-            if(dindex < 4)
+            if(dindex < 3)
                 return &vcom_strings[dindex];
     }
     return NULL;
