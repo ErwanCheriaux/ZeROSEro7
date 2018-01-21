cd1cbf9c0cefddd5b841029921a0462266e68eec
Erwan CHERIAUX <erwan.cheriaux@telecom-paristech.fr>
2018-01-21 20:22:36 +0100

    spi config valid

--- software/usb_sniffer/STM32/src/spi.c
+++ software/usb_sniffer/STM32/src/spi.c
@@ -37,63 +37,28 @@ static uint8_t  rxbuf[512];
 void spi_init(void)
 {
     /*
-   * SPI3 I/O pins setup.
-   */
+     * SPI3 I/O pins setup.
+     */
     palSetPadMode(GPIOC, GPIOC_SPI3_SCK, PAL_MODE_ALTERNATE(6));
     palSetPadMode(GPIOC, GPIOC_SPI3_MISO, PAL_MODE_ALTERNATE(6));
     palSetPadMode(GPIOC, GPIOC_SPI3_MOSI, PAL_MODE_ALTERNATE(6));
     palSetPadMode(GPIOA, GPIOA_SPI3_NSS, PAL_MODE_INPUT_PULLDOWN);
 
     /*
-     * Clock
+     * CONFIG SPI
+     * 8bit
+     * CPHA = 0, CPOL = 0
+     * MSB-first
+     * NSS Hardware
+     * Motorola mode
+     * Slave mode
+     * Rx interupt on
      */
-    //GPIOA
-    RCC_AHB1_RSTR |= (1 << 0);   //GPIOARST = 1
-    RCC_AHB1_ENR |= (1 << 0);    //GPIOAEN = 1
-    RCC_AHB1_LPENR |= (1 << 0);  //GPIOALPEN = 1
-    //SPI3
-    RCC_APB1_RSTR |= (1 << 15);   //SPI3RST = 1
-    RCC_APB1_ENR |= (1 << 15);    //SPI3EN = 1
-    RCC_APB1_LPENR |= (1 << 15);  //SPI3LPEN = 1
 
-    SPI3_CR1 &= ~(1 << 6);  //SPE = 0  : Turn off SPI before config
-
-    /* 1. Set the DFF bit to define 8- or 16-bit data frame format */
-    SPI3_CR1 &= ~(1 << 11);  //DFF = 0 : 8bit
-
-    /* 2. Select the CPOL and CPHA bits to define one of the four relationships between the
-    data transfer and the serial clock (see Figure 248). For correct data transfer, the CPOL
-    and CPHA bits must be configured in the same way in the slave device and the master
-    device. This step is not required when the TI mode is selected through the FRF bit in
-    the SPI_CR2 register. */
-    SPI3_CR1 &= ~(1 << 0);  //CPHA = 0
-    SPI3_CR1 &= ~(1 << 1);  //CPOL = 0
-
-    /*3. The frame format (MSB-first or LSB-first depending on the value of the LSBFIRST bit in
-    the SPI_CR1 register) must be the same as the master device. This step is not
-    required when TI mode is selected. */
-    SPI3_CR1 &= ~(1 << 7);  //LSBFIRST = 0 : MSB-first
-
-    /* 4. In Hardware mode (refer to Slave select (NSS) pin management on page 880), the
-    NSS pin must be connected to a low level signal during the complete byte transmit
-    sequence. In NSS software mode, set the SSM bit and clear the SSI bit in the SPI_CR1
-    register. This step is not required when TI mode is selected. */
-    //SPI3_CR1 |= (1 << 9);   //SSM = 1
-    //SPI3_CR1 &= ~(1 << 8);  //SSI = 0
-
-    SPI3_CR1 &= ~(1 << 9);  //SSM = 0 : NSS Hardware
-
-    /* 5. Set the FRF bit in the SPI_CR2 register to select the TI mode protocol for serial
-    communications. */
-    SPI3_CR2 &= ~(1 << 4);  //FRF = 0 : Motorola mode
-
-    //turn on Rx interrupt
-    SPI3_CR2 |= (1 << 6);  //RXNEIE = 1
-
-    /* 6. Clear the MSTR bit and set the SPE bit (both in the SPI_CR1 register) to assign the
-    pins to alternate functions. */
-    SPI3_CR1 &= ~(1 << 2);  //MSTR = 0 : Slave mode
-    SPI3_CR1 |= (1 << 6);   //SPE = 1  : Turn on SPI
+    RCC->APB1ENR |= RCC_APB1ENR_SPI3EN;  // Turn SPI3 clock on.
+    SPI3->CR1 = 0;                       // SPI off while configuring it
+    SPI3->CR2 = SPI_CR2_RXNEIE;          // RX interrupt if data present
+    SPI3->CR1 = SPI_CR1_SPE;             // Enable SPI in slave mode.
 
     //debug
     spi_display_config();
