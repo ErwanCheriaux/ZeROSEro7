5b1c199b8e769ca9f8cfd752ba39c342b9ee8a70
Enguerrand de RIBAUCOURT <deribaucourt@telecom-paristech.fr>
2018-01-23 18:34:38 +0100

    USB Sniffer app file dumping

--- smartphone/BLEParrot/usbsniffer/src/main/java/fr/telecom_paristech/usbsniffer/USBSnifferConnectedActivity.java
+++ smartphone/BLEParrot/usbsniffer/src/main/java/fr/telecom_paristech/usbsniffer/USBSnifferConnectedActivity.java
@@ -1,6 +1,8 @@
 package fr.telecom_paristech.usbsniffer;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.os.Environment;
 import android.text.method.ScrollingMovementMethod;
 import android.util.Log;
 import android.view.KeyEvent;
@@ -8,17 +10,33 @@ import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
 import blecommon.ConnectedActivity;
 
 public class USBSnifferConnectedActivity extends ConnectedActivity {
 
-
     private TextView logWindow;
+    public static final String FILENAME = "usb_sniffer_dump.txt";
+
+    private FileOutputStream dumpFileStream;
 
     @Override
     public void onNotificationReceived(String msg) {
         logWindow.append("<- " + msg + "\n");
         logWindow.scrollTo(0, 0);
+
+        dumpInFile(msg);
+    }
+
+    private void dumpInFile(String s) {
+        try {
+            dumpFileStream.write(s.getBytes());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
@@ -27,9 +45,34 @@ public class USBSnifferConnectedActivity extends ConnectedActivity {
         setContentView(R.layout.activity_usbsniffer_connected);
 
         logWindow = (TextView) findViewById(R.id.logWindow);
-
-
-
         logWindow.setMovementMethod(new ScrollingMovementMethod());
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+
+        String path = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS) + File.separator;
+        File dumpFile = new File(path, FILENAME);
+        if(dumpFile.exists()) {
+            dumpFile.delete();
+        }
+        try {
+            dumpFile.createNewFile();
+            dumpFileStream = openFileOutput(FILENAME, Context.MODE_PRIVATE);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void onStop() {
+
+        try {
+            dumpFileStream.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        super.onStop();
     }
 }
